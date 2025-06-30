3f8e6078e752d0ad6a961f68fab4bc4091606975
tayljr <jrtay04@gmail.com>
2025-06-30 15:12:10 +1000

    Player can now move
    movement controls and jump controls

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -2,15 +2,85 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    public CharacterController controller;
+
+    public float speed = 10f;
+    public float sprintModifier = 1.5f;
+    public float crouchModifier = 0.5f;
+
+    public float jumpForce = 2f;
+    public bool fastFalling = true;
+    public float fallingModifier = 1.5f;
+    public float gravity = 10f;
+
+    private Vector3 moveDir = Vector3.zero;
+    private float speedModifier = 1f;
+    [SerializeField]
+    private float verticalVelocity = 0f;
+
+    [SerializeField]
+    private bool grounded = false;
+
+    public void Move(Vector2 dir)
+    {
+        moveDir = new Vector3(dir.x, moveDir.y, dir.y);
+        //controller.Move(moveDir);
+    }
+
+    public void Jump()
+    {
+        Debug.Log(grounded);
+        if (grounded)
+        {
+            verticalVelocity = jumpForce;
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
+        if (other.gameObject != gameObject)
+        {
+            grounded = false;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject != gameObject)
+        {
+            verticalVelocity = 0;
+            grounded = true;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //ground check
+        /*
+        RaycastHit hit;
+        Vector3 feetPos = transform.position;
+        feetPos.y -= controller.height / 3;
+        grounded = Physics.SphereCast(feetPos, controller.height / 4, -transform.up, out hit, 0.5f);
+        */
+        moveDir.y = verticalVelocity;
+        controller.Move(moveDir * speed * speedModifier * Time.deltaTime);
+        if (!grounded)
+        {
+            if (fastFalling && verticalVelocity < 0)
+            {
+                verticalVelocity -= gravity * fallingModifier * Time.deltaTime;
+            }
+            else
+            {
+                verticalVelocity -= gravity * Time.deltaTime;
+            }
+        }
         
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,64 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public CharacterMovement characterMovement;
+    public InputSystem_Actions playerControls;
+
+    private InputAction move;
+    private InputAction jump;
+    private InputAction look;
+
+    private void Awake()
+    {
+        playerControls = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        move = playerControls.Player.Move;
+        move.Enable();
+        move.performed += Move;
+        move.canceled += Move;
+        jump = playerControls.Player.Jump;
+        jump.Enable();
+        jump.performed += Jump;
+        look = playerControls.Player.Look;
+        look.Enable();
+        look.performed += Look;
+    }
+
+    private void Look(InputAction.CallbackContext obj)
+    {
+        //Debug.Log(obj.ReadValue<Vector2>());
+    }
+
+    private void Jump(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Jump");
+        characterMovement.Jump();
+    }
+    private void Move(InputAction.CallbackContext obj)
+    {
+        //Debug.Log(obj.ReadValue<Vector2>());
+
+        characterMovement.Move(obj.ReadValue<Vector2>());
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+        move.performed -= Move;
+        move.canceled -= Move;
+        jump.Disable();
+        jump.performed -= Jump;
+        look.Disable();
+        look.performed -= Look;
+    }
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
