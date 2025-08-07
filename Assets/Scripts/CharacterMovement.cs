9f81dc8f0c665454fac652b32067b61785bed25c
tayljr <jrtay04@gmail.com>
2025-08-07 16:35:58 +1000

    working on gliding

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -14,6 +14,7 @@ public class CharacterMovement : MonoBehaviour
     public float sprintModifier = 1.5f;
     public float crouchModifier = 0.5f;
     private bool isCrouching = false;
+    private bool isGliding = false;
 
     public int defaultJumpCount = 1;
     public int maxJumpCount = 1;
@@ -23,9 +24,13 @@ public class CharacterMovement : MonoBehaviour
     public float fallingModifier = 1.5f;
     public float gravity = 10f;
     public Vector3 climbSpeed = new Vector3(10f, 10f, 10f);
+    public float glideGrav = 25f;
+    public float glideForwardSpeed = 10f;
+
 
     //will be private
     public bool canClimb;
+    public bool canGlide = true;
 
     private Vector3 moveDir = Vector3.zero;
     private float speedModifier = 1f;
@@ -136,6 +141,25 @@ public class CharacterMovement : MonoBehaviour
         canClimb = enableClimb;
     }
 
+    public void SetCanGlide(bool enableGlide)
+    {
+        canGlide = enableGlide;
+    }
+
+    public void Glide(bool startGliding)
+    {
+        if (canGlide && !grounded)
+        {
+            isGliding = startGliding;
+            Move(new Vector2(moveDir.x, 1));
+        }else if (grounded)
+        {
+            isGliding = false;
+            //todo not stoppping
+            Move(Vector2.zero);
+        }
+    }
+
     //double jump
     public void SetJumpCount(int newCount)
     {
@@ -155,7 +179,6 @@ public class CharacterMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void OnEnable()
@@ -188,6 +211,7 @@ public class CharacterMovement : MonoBehaviour
             grounded = true;
             jumpCount = 0;
             groundCount++;
+            isGliding = false;
         }
     }
 
@@ -201,6 +225,7 @@ public class CharacterMovement : MonoBehaviour
         feetPos.y -= controller.height / 3;
         grounded = Physics.SphereCast(feetPos, controller.height / 4, -transform.up, out hit, 0.5f);
         */
+
         Vector3 worldMoveDir = head.transform.TransformDirection(moveDir);
         worldMoveDir = worldMoveDir * speed * speedModifier;
         worldMoveDir.y = verticalVelocity;
@@ -215,6 +240,13 @@ public class CharacterMovement : MonoBehaviour
             }
             worldMoveDir = Vector3.Scale(transform.TransformDirection(moveDir.x, moveDir.z, forwardMoveDir), climbSpeed);
         }
+
+        if(isGliding)
+        {
+            //worldMoveDir.y = -glideGrav;
+            worldMoveDir = transform.TransformDirection(moveDir.x, -glideGrav, moveDir.z * glideForwardSpeed);
+        }
+
         controller.Move(worldMoveDir * Time.deltaTime);
         if (!grounded)
         {
