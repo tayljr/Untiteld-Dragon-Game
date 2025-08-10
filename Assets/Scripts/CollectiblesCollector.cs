abf77a04bd5d167f384213c97d5c51e49f5ce2a0
Thomas <1032861@student.sae.edu.au>
2025-08-10 14:46:34 +1000

    added working district gates, updated currency and began loot table

--- Assets/Scripts/CollectiblesCollector.cs
+++ Assets/Scripts/CollectiblesCollector.cs
@@ -8,12 +8,19 @@ public class CollectiblesCollector : MonoBehaviour
 
     [SerializeField] private int Collectibles = 0;
 
-    [SerializeField] private int Keys = 0;
+    [SerializeField] public int Keys = 0;
 
 
     public TextMeshProUGUI collectibleText;
 
+    public TextMeshProUGUI currencyText;
+
     public TextMeshProUGUI KeysText;
+
+    public int currencyCount;
+    public CurrencyManager cm;
+    public Coin coin;
+   
 
 
 
@@ -31,8 +38,16 @@ public class CollectiblesCollector : MonoBehaviour
 
         if (other.transform.tag == "Key")
         {
+            //km.KeyCount++;
             Keys++;
             KeysText.text = "Key : " + Keys.ToString();
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Currency"))
+        {
+            currencyCount += coin.coinValue;
+            currencyText.text = "currency: " + currencyCount.ToString();
             Destroy(other.gameObject);
         }
 
---
+++ Assets/Scripts/KeyManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class KeyManager : MonoBehaviour
+{
+
+   
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+          
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+}
