aaaa9282089b2115483720d4cdb3a45bc191f49a
TheLastSlice898 <56330886+TheLastSlice898@users.noreply.github.com>
2025-10-21 13:21:49 +1100

    Fixed Spawner and enemy logic/ Added Knockback
    Still need to do some fine adjustment to the enemy pathfinding and Player detection

---
+++ Assets/Knockback.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class Knockback : MonoBehaviour
+{
+    public AnimationCurve knockbackCurve;
+    public float knockbackStrength = 5f;
+    public float knockbackDuration = 0.5f;
+    private float knockbackTimer;
+    private void OnEnable()
+    {
+        HealthBase.OnDamage += HealthBase_OnDamage;
+    }
+
+    private void HealthBase_OnDamage(float damage, string tag)
+    {
+        if (tag == "Enemy")
+        {
+            StartCoroutine(KnockbackCorutine(damage, tag));
+        }
+    }
+
+    IEnumerator KnockbackCorutine(float damage, string tag)
+    {
+        //enemy knockback logic
+        if (tag == "Enemy")
+        {
+            var Charcontroller = GetComponent<CharacterController>();
+            knockbackTimer = knockbackDuration;
+            while (knockbackTimer > 0)
+            {
+                float curveKnockback = knockbackCurve.Evaluate(knockbackDuration / knockbackTimer);
+                Vector3 knockbackDirection = -transform.forward;
+                Charcontroller.Move(knockbackDirection * knockbackStrength * Time.deltaTime);
+                knockbackTimer -= Time.deltaTime;
+                yield return null;
+            }
+        }
+            
+
+
+        //player knockback logic
+    }
+}
